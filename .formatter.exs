8a68b975ae1d698fef5f4fa9724feb91d7546fa2
Thomas Brewer <thomas.brewer@hatchcare.com>
2026-05-11 04:58:13 -0400

    chore: add code quality tooling (credo, ex_dna, ex_slop, doctor, dialyxir)

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 # Used by "mix format"
 [
+  import_deps: [:credo],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
--- mix.exs
+++ mix.exs
@@ -8,7 +8,18 @@ defmodule CanOpener.MixProject do
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      aliases: aliases(),
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+        plt_add_apps: [:ex_unit]
+      ]
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [check: :test, precommit: :test]
     ]
   end
 
@@ -21,11 +32,33 @@ defmodule CanOpener.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
+  defp aliases do
+    [
+      check: [
+        "compile --warnings-as-errors",
+        "deps.unlock --unused",
+        "format --check-formatted",
+        "credo --strict",
+        "doctor",
+        "dialyzer",
+        "test"
+      ],
+      precommit: ["check"]
+    ]
+  end
+
   defp deps do
     [
       {:req, "~> 0.5"},
       {:jason, "~> 1.4"},
-      {:mimic, "~> 2.0", only: :test}
+      {:mimic, "~> 2.0", only: :test},
+
+      # Code Quality (dev/test only)
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_dna, github: "dannote/ex_dna", branch: "master", only: [:dev, :test], runtime: false},
+      {:ex_slop, github: "dannote/ex_slop", branch: "master", only: [:dev, :test], runtime: false},
+      {:doctor, "~> 0.21", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
